0f69df73725cf7bade0492306e9a95e75984f0ed
Ameen <ameen2607@gmail.com>
2024-09-09 18:08:55 +0530

    updated stop container

--- stop_container.sh
+++ stop_container.sh
@@ -1,4 +1,12 @@
+
+
+#containerID = `docker ps | awk -F "" '{Print $1}'`
+#docker rm -f $containerID 
+
 #!/bin/bash
 
-containerID = `docker ps | awk -F "" '{Print $1}'`
-docker rm -f $containerID 
+# Get the container ID of the most recently started container
+containerID=$(docker ps -l -q)
+
+# Stop and remove the most recent container
+docker rm -f $containerID
